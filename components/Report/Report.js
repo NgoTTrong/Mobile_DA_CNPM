ff3a94341fc10af977cdf857f0f5bb935c11b190
NgoTTrong <trongngo08082008@gmail.com>
2023-04-09 21:59:36 +0700

    fixed detail chart

--- components/Report/Report.js
+++ components/Report/Report.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 import{
   View,
@@ -9,11 +9,82 @@ import{
 import Chart from '../Home/Chart'
 import { Dimensions } from "react-native";
 import ChartReport from './ChartReport';
+import FetchData from '../../Utils/FetchData';
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 export default Report = (props) => {
-  const {navigation, route} = props;
+
+    const {navigation, route} = props;
+    const [data, setData] = useState({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          data: [0, 1, 2, 1, 2, 2, 2, 2, 2, 0],
+          color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+          strokeWidth: 2, // optional
+        },
+      ],
+      legend: ['Strawberry-stauts'], // optional
+    });
+    if (route.params.name === "Temperature"){
+      useEffect(() => {
+        const getData = async () => {
+          setData({
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: (await FetchData('temperature-sensor')).map((e) =>
+                parseInt(e)
+              ),
+              color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+              strokeWidth: 2, // optional
+            },
+          ],
+          legend: ['Temperature'], // optional
+        });
+        }
+        getData();
+      }, []);
+    }else if(route.params.name === "Humidity"){
+            useEffect(() => {
+        const getData = async () => {
+          setData({
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: (await FetchData('humidity-sensor')).map((e) =>
+                parseInt(e)
+              ),
+              color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+              strokeWidth: 2, // optional
+            },
+          ],
+          legend: ['Humidity'], // optional
+        });
+        }
+        getData();
+      }, []);
+    }else if (route.params.name === "Light intensity"){
+        useEffect(() => {
+        const getData = async () => {
+          setData({
+          key: 4,
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: (await FetchData('light-sensor')).map((e) => parseInt(e)),
+              color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+              strokeWidth: 2, // optional
+            },
+          ],
+          legend: ['Light'], // optional
+        });
+        }
+        getData();
+      }, []);
+    }
+
   const months = [
     {
       key: 1,
@@ -39,7 +110,7 @@ export default Report = (props) => {
         keyExtractor={item=>item.key}
         contentContainerStyle={styles.monthContainer}
       />
-      <ChartReport name={route.params.name}/>
+      <ChartReport name={route.params.name} data={data}/>
   </View>
 }
 
